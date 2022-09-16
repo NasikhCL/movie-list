8278d9a074b0c9b1f77cb8286cf14fc9767f6612
NasikhCL <nasikcl@gmail.com>
2022-09-17 00:32:43 +0530

    final stage

---
+++ details.js
@@ -0,0 +1,138 @@
+const mainContainer =document.getElementById('main-container')
+function showDetails(){
+const url = window.location.href
+
+console.log(url)
+const strs = url.split('?');
+const id = strs.at(-1)
+console.log(id)
+fetchMovieDetails(id)
+
+
+}
+
+
+
+async function fetchMovieDetails(id) {
+     
+    
+    // console.log(query)
+    const API_KEY = "291bdbfa";
+    const request = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`);
+    const response = await request.json();
+    // const movies = response.Search;
+    console.log(response)
+
+    getMovieDetails(response)
+    
+
+}
+// {Title: 'Sita Ramam', Year: '2022', Rated: 'N/A', Released: '05 Aug 2022', Runtime: '2 min', …}
+// Actors
+// : 
+// "Dulquer Salmaan, Mrunal Thakur, Rashmika Mandanna"
+// Awards
+// : 
+// "N/A"
+// BoxOffice
+// : 
+// "N/A"
+// Country
+// : 
+// "India"
+// DVD
+// : 
+// "N/A"
+// Director
+// : 
+// "Hanu Raghavapudi"
+// Genre
+// : 
+// "Action, Drama, Mystery"
+// Language
+// : 
+// "Telugu"
+// Metascore
+// : 
+// "N/A"
+// Plot
+// : 
+// "An orphan soldier, Lieutenant Ram's life changes, after he gets a letter from a girl named Sita. He meets her and love blossoms between them. When he comes back to his camp in Kashmir, he sends a letter to Sita which won't reach her."
+// Poster
+// : 
+// "https://m.media-amazon.com/images/M/MV5BN2RjZDJhYzUtOTQ5Yy00OWM3LWE5OTctM2Y0YWVmNzAzODllXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_SX300.jpg"
+// Production
+// : 
+// "N/A"
+// Rated
+// : 
+// "N/A"
+// Ratings
+// : 
+// [{…}]
+// Released
+// : 
+// "05 Aug 2022"
+// Response
+// : 
+// "True"
+// Runtime
+// : 
+// "2 min"
+// Title
+// : 
+// "Sita Ramam"
+// Type
+// : 
+// "movie"
+// Website
+// : 
+// "N/A"
+// Writer
+// : 
+// "Hanu Raghavapudi, Jay Krishna, Raj Kumar Kandamudi"
+// Year
+// : 
+// "2022"
+// imdbID
+// : 
+// "tt20850406"
+// imdbRating
+// : 
+// "8.1"
+// imdbVotes
+// : 
+// "13,652"
+
+function getMovieDetails(details){
+    // movies.forEach(element => {
+    // console.log(main)
+    let detailsContainer = document.createElement('div')
+    detailsContainer.classList.add('details-page-container');
+    
+    detailsContainer.innerHTML = `<div class="movie-poster">
+                                <img src=${details.Poster} class="poster-image" alt="">
+                            </div>
+                            <div class="movie-details-container">
+                                <div class="movie-details">
+                                    <h1>${details.Title}</h1>
+                                    <p><span>${details.Year}</span> <span>${details.Runtime}</span> <span>${details.imdbRating}</span> <span>${details.Country}</span></p>
+                                    <p>${details.Plot}</p>
+                                    <p>Genre: ${details.Genre}</p>
+                                    <p>language: ${details.Language}</p>
+                                    <p>Director:   ${details.Director}</p>
+                                    <p>Writers:   ${details.Writer}</p>
+                                    <p>Actors:  ${details.Actors}</p>
+                                </div>
+                                
+                            </div>`
+    mainContainer.appendChild(detailsContainer);
+    // let carddocument.getElementsByClassName('card').addEventListener('click', showDetails(element.imdbID))
+    
+
+}
+
+
+// http://www.omdbapi.com/?i=tt1349853
+
+
